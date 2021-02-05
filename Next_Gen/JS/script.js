3f806d96b28e1f687ebe42b45b33e3707d71df66
mo2rayo <70894876+mo2rayo@users.noreply.github.com>
2021-02-05 10:37:17 +0100

    Project 7, 8 and 9
    Project I worked on page 7, 8 and 9
    -Page 7 is all about OUR VISION and all (index.html)
    -Page 8 and 9 contain OUR LEADERSHIP TEAM next.html)

---
+++ Next_Gen/JS/script.js
@@ -0,0 +1,6 @@
+// slide-up script
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+    // removing smooth scroll on slide-up button click
+    $('html').css("scrollBehavior", "auto");
+});
